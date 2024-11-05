29e3aee1afc73f088e9d0133b7ae5e94a664dd80
Dana Kaziyeva <119665823+Dana-Kaziyeva@users.noreply.github.com>
2024-11-05 23:26:33 +0500

    Create 674.py

---
+++ task_1/674.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        n = 1
+        longest = 1
+        for i in range(0, len(nums) - 1):  
+            if nums[i] < nums[i + 1]:
+                n += 1
+            else:
+                longest = max(n, longest)
+                n = 1
+
+        return max(n, longest)
